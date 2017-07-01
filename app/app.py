452381ccca4ea01992b4e8dc231b6efc63c0180e
Matthew Turner <maturner01@gmail.com>
2017-07-01 13:32:47 +0100

    working

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-import json
+import os
 import uuid
 
+from glob import glob
 from pandas import DataFrame
 
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, request
 
 from requests import get
 
@@ -22,17 +23,19 @@ def hello():
 @app.route('/export', methods=['POST'])
 def export():
 
+    # remove any previously-exported files
+    glb = glob('app/static/tmp/*')
+    for f in glb:
+        os.remove(f)
+
+    # get search results in json format
+    search_url = request.form['iatv-url']
+
     tmp_csv = 'app/static/tmp/' + str(uuid.uuid4()) + '.csv'
 
     # replace with URL passed in request, tag output=json, and request it
-    res = get('http://localhost:5000/archive-mock')
+    res = get(search_url + '&output=json&rows=1000')
 
     DataFrame(data=res.json()).to_csv(tmp_csv, index=False)
 
     return redirect(tmp_csv.replace('app/', ''))
-
-
-@app.route('/archive-mock')
-def archive_mock():
-
-    return jsonify(json.loads(open('strangle-economy-ex.json').read()))
